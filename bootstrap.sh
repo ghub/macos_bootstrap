4a1691d4bf51ed8393dcb30e7a4a008b7d5b4650
Ge Wang <code.gewang@gmail.com>
2017-11-22 06:26:20 -0700

    Use brew bundle and sdk 9.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,31 +2,4 @@
 
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew tap caskroom/cask
-brew tap caskroom/drivers
-
-brew install bash-completion
-brew install cmake
-brew install macvim
-brew install packer
-brew install reattach-to-user-namespace
-brew install stow
-brew install tmux
-
-brew cask install cuda
-brew cask install docker
-brew cask install firefox
-brew cask install gfxcardstatus
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install keepassx
-brew cask install skype
-brew cask install the-unarchiver
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install virtualbox-extension-pack
-brew cask install vlc
-
-brew tap homebrew/science
-brew cask install xquartz
-brew install octave
+brew bundle
