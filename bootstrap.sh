d11f84dadbc1f1b416617deb7cb0363b8a42fcb8
Ge Wang <code.gewang@gmail.com>
2017-11-23 08:00:03 -0700

    Split Brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew bundle
+cat Brewfile.d/* | sort -u | brew bundle --verbose --file=-
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ev
 
 for each in Brewfile.d/*; do
-    brew bundle --verbose --file=$each
+    time brew bundle --verbose --file=$each
 done
